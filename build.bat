5bb099318702f952d04608bfc8f7e2e25d905692
That Blair Guy <thatblairguy@users.noreply.github.com>
2018-09-13 00:53:27 -0400

    Fix image in the header banner.

--- build.bat
+++ build.bat
@@ -1,5 +1,8 @@
-mkdir docs\assets\
+mkdir docs\assets\ 2> nul
 
 copy src\manifest.json  docs\
 copy src\*.htm          docs\
 copy src\assets\*       docs\assets\
+
+copy src\layout\bootstrap.min.* docs\layout\
+copy src\layout\layout.css      docs\layout\
